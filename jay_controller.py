707a95afbe68ba22f0d0373846a6ebfd609343c1
Jay Barot <barotjay998@yahoo.com>
2022-12-10 13:39:13 -0600

    c mapping init

--- jay_controller.py
+++ jay_controller.py
@@ -1,7 +1,17 @@
 import docker
-client = docker.from_env() # Lets you do anything the docker command does, but from within Python apps
+# Lets you do anything the docker command does, but from within Python apps
+docker_client = docker.from_env()
 
-services = client.services.list()
+# Get all the networks and services in the swarm
+networks = docker_client.networks.list()
+services = docker_client.services.list()
+
+# Store the network IDs
+network_ids = []
+for network in networks:
+    network_ids.append(network.id)
+
+# Get all my services running in the swarm
 my_services = []
 my_service_ids = set()
 for service in services:
@@ -9,16 +19,10 @@ for service in services:
         my_services.append(service)
         my_service_ids.add(service.id)
 
-print("All Services: {0}".format(len(my_services)))
-print(my_services)
-
-networks = client.networks.list()
-
-graph ={}
-network_ids = []
-for network in networks:
-    network_ids.append(network.id)
-
-print("\n")
-print("All Network Ids: {0}".format(len(network_ids)))
-print(network_ids)
+# Get the mapping
+def get_service_map(my_services):
+    service_networks = {} # Stores the networks for each service.
+    service_map = {}
+    for service in my_services:
+        service_networks[service] = [dictionary["Target"] for dictionary in service.attrs["Spec"]["TaskTemplate"]["Networks"]]
+        print(service_networks, "\n")
