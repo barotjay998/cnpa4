59d48443939fd65e2344f4645bd89083e15476bd
Jay Barot <barotjay998@yahoo.com>
2022-12-10 13:53:32 -0600

    c mapping 4

--- jay_controller.py
+++ jay_controller.py
@@ -28,9 +28,21 @@ def get_service_map(my_services):
     for service in my_services:
         service_networks[service] = [dictionary["Target"] for dictionary in service.attrs["Spec"]["TaskTemplate"]["Networks"]]
 
+    # Assign the key values of the service_map as the service Ids
     for service in service_networks:
         service_map[service] = []
     
-    print(service_map, " ", len(service_map))
+    for service in service_networks:
+        for network in service_networks[service]:
+            for linked_service in service_networks:
+                if linked_service == service: 
+                    continue
+                if network in service_networks[linked_service]:
+                    if linked_service not in service_map[service]:
+                        service_map[service].append(linked_service)
+                    if service not in service_map[linked_service]:
+                        service_map[linked_service].append(service)
 
-get_service_map(my_services)
+    return service_map    
+
+print(get_service_map(my_services))
