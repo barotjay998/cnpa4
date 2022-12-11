2737cfcd9594e5dea8b39e4561e0469e71c72eff
Jay Barot <barotjay998@yahoo.com>
2022-12-10 19:09:49 -0600

    c mapping 26

--- jay_controller.py
+++ jay_controller.py
@@ -179,14 +179,16 @@ def calculate_cost(service_and_networks):
     for network in networks:
         networks_and_service[network["ID"]] = []
     
-    # for network in networks_and_service:
+    print(networks_and_service)
+    for network in networks_and_service:
+        print(network)
     #     for service in service_and_networks:
     #         print(service)
             #for i in service:
                 #if i == network:
                     #networks_and_service[network].append(service)
 
-    print(networks_and_service)
+    
     return None
 
 service_and_neighbours, service_and_networks = get_service_map(my_services)
